206279d3ecbfc8006fc77df282094f892af6223b
Noitidart <noitidart@gmail.com>
2016-09-21 06:32:14 -0700

    Update watchify.js
    get path to xpi to download and check

--- watchify.js
+++ watchify.js
@@ -1,1 +1,4 @@
-alert('hi on page!')
+var addonid = document.querySelector('[data-addonid]').getAttribute('data-addonid');
+var addonslug = window.location.href.match(/addon\/[^/]+/)[1];
+var addonxpi = 'https://addons.mozilla.org/firefox/downloads/latest/' + addonslug + '/addon-' + addonid + '-latest.xpi';
+alert('path to xpi:' + addonxpi);
