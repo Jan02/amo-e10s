52804e5aac98db5b091f799c4957543d2a782d0e
Noitidart <noitidart@gmail.com>
2016-09-21 06:37:03 -0700

    addonslug was undefined

--- watchify.js
+++ watchify.js
@@ -1,4 +1,4 @@
 var addonid = document.querySelector('[data-addonid]').getAttribute('data-addonid');
-var addonslug = window.location.href.match(/addon\/[^/]+/)[1];
+var addonslug = window.location.href.match(/addon\/([^/]+)/)[1];
 var addonxpi = 'https://addons.mozilla.org/firefox/downloads/latest/' + addonslug + '/addon-' + addonid + '-latest.xpi';
 alert('path to xpi:' + addonxpi);
